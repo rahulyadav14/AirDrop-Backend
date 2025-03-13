d5e45f6e31e1dbf131ac67edad18f79821315d37
Rahul <y.rahul.14082002@gmail.com>
2025-03-14 04:40:40 +0530

    Initial commit - Spring Boot setup

---
+++ airDrop-clone/src/main/java/com/example/airdropclone/AirDropCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.airdropclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirDropCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirDropCloneApplication.class, args);
+	}
+
+}
